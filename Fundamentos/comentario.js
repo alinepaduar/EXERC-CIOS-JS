64ec59a356353fce9d2e82a2fa01ff4337d74833
Aline Pádua <paduarosa.aline@gmail.com>
2018-04-18 21:40:57 -0300

    Initial commit

---
+++ Fundamentos/comentario.js
@@ -0,0 +1,6 @@
+// Comentário de uma linha
+
+/* 
+    Comnetário de
+    múltiplas linhas 
+*/
---
+++ Fundamentos/organizacao.js
@@ -0,0 +1,9 @@
+/* // Código javascript é organizado em sentença de código.
+console.log('Sentença de código');
+
+// Código javascript é organizado em blocos de código ( {} ) para agrupar as sentenças de código.
+{
+    console.log('Olá');
+    console.log('Mundo'); 
+} */
+
