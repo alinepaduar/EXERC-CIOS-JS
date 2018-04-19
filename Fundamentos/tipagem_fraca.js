d307726cf575309a587d543124ee5474e8460069
Aline Pádua <paduarosa.aline@gmail.com>
2018-04-18 22:08:36 -0300

    Tipagem Fraca
    Aula sobre Tipagem Fraca em JS.

---
+++ Fundamentos/tipagem_fraca.js
@@ -0,0 +1,9 @@
+let qualquer = 'Legal';
+// Typeof mostra qual a tipagem da variável criada.
+console.log(typeof qualquer);
+
+qualquer = 3.50;
+// Typeof mostra qual a tipagem da variável criada.
+console.log(typeof qualquer);
+
+// Evitar nome genéricos e siglas para variáveis e constantes.
