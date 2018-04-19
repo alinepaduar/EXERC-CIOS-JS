ad0879a2a5a44b9e0d221335e9a74ef477862518
Aline Pádua <paduarosa.aline@gmail.com>
2018-04-18 22:33:44 -0300

    Objeto Math
    Aula sobre objeto Math JS.

---
+++ Fundamentos/math.js
@@ -0,0 +1,8 @@
+/* Math é um objeto
+    que possui uma constante chamada PI
+    e uma função que calcula potência (pow)
+*/
+const raio = 5.6;
+const area = Math.PI * Math.pow(raio,2);
+console.log(area);
+console.log(typeof Math);
