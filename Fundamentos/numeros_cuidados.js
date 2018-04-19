834e2c3dc8cf18eec58c9e4fcf801ddba6d741ca
Aline Pádua <paduarosa.aline@gmail.com>
2018-04-18 22:27:39 -0300

    Números - Cuidados
    Aula sobre alguns cuidados com números JS.

---
+++ Fundamentos/numeros_cuidados.js
@@ -0,0 +1,14 @@
+// Valor infinito.
+console.log(7/0);
+
+// Operação com string.
+console.log('10'/2);
+
+// Gera NaN
+console.log('Show!' * 2);
+
+// Gera imprecisão
+console.log(0.1 + 0.7);
+
+// Não pode chamar o literal direto a função.
+// console.log(10.toString());
