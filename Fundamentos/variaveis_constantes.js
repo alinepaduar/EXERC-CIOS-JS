928b4ea33e4562fa764e6cc04b7358a130beeee7
Aline Pádua <paduarosa.aline@gmail.com>
2018-04-18 22:00:26 -0300

    Variáveis e Constantes
    Aula de Variáveis e Constantes

---
+++ Fundamentos/variaveis_constantes.js
@@ -0,0 +1,20 @@
+// Variável VAR
+var a = 3;
+// Pode ser declarada mais de um vez.
+var a = 30;
+
+console.log(a);
+
+// Variável LET - Melhor opção.
+let b = 4;
+// Declarada somente uma vez;
+b = 40;
+
+console.log(b);
+
+// Constante - Não pode ser alterada.
+const c = 5;
+
+console.log(c);
+
+
