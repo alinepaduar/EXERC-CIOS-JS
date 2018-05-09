6736fcc713ae66521589b55b7b8d77e4fa3dcf30
Aline Pádua <paduarosa.aline@gmail.com>
2018-05-08 21:38:08 -0300

    Variável VAR em loop
    Aula sobre laço de repetição com a variável VAR,  onde se é inserido funções com o valor da variável i, mas o VAR é visto como global por não estar dentro de uma função e repetindo sempre o último valor em todas as funções das posições do array criado.

--- Fundamentos/variavel_var_loop_02.js
+++ Fundamentos/variavel_var_loop_02.js
@@ -1,3 +1,10 @@
+/*
+    Aula sobre laço de repetição com a variável VAR, 
+    onde se é inserido funções com o valor da variável i, 
+    mas o VAR é visto como global por não estar dentro de uma função e 
+    repetindo sempre o último valor em todas as 
+    funções das posições do array criado.
+*/
 const funcs = [];
 
 for (var i = 0; i < 10; i++){
