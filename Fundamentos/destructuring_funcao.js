a7b6fb0fcdf589df03e55fabaa03ca2d2e75eda0
Aline Pádua <paduarosa.aline@gmail.com>
2018-05-27 20:47:04 -0300

    Destructuring Função
    Aula sobre Destructuring Função em JS.

---
+++ Fundamentos/destructuring_funcao.js
@@ -0,0 +1,9 @@
+function return_num({ min = 0, max = 1000 }) {
+    const valor = Math.random() * (max - min) + min;
+    return Math.floor(valor);
+}
+
+const obj = { max: 50, min: 40 }
+console.log(return_num(obj));
+console.log(return_num({ min: 955 }));
+console.log(return_num({}));
