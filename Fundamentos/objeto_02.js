dba25cf96308c8d7d89c0ce7c64c2374d095555e
Aline Pádua <paduarosa.aline@gmail.com>
2018-05-27 17:33:18 -0300

    Estanciando Função
    Aula sobre estanciar função em JS.

---
+++ Fundamentos/objeto_02.js
@@ -0,0 +1,10 @@
+console.log(typeof Object);
+console.log(typeof new Object); // Estanciando a função
+
+const Cliente = function() {}
+console.log(typeof Cliente);
+console.log(typeof new Cliente); // Estanciando a função
+
+class Produto {}
+console.log(typeof Produto);
+console.log(typeof new Produto()); // Estanciando a função
