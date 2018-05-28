c3be522d139d9ddd4484eb8bf8a43647fe44e507
Aline de Pádua <paduarosa.aline@gmail.com>
2018-05-28 15:55:46 -0300

    IF 02
    Aula sobre IF em JS.

---
+++ Controle/if_02.js
@@ -0,0 +1,28 @@
+function teste_01 (num) {
+
+    if (num > 7)
+        console.log(num);
+
+    console.log('Final');
+
+}
+
+teste_01(6);
+teste_01(8);
+
+function teste_02 (num) {
+
+    // if (num > 7); {
+    //     console.log(num);
+    // }
+
+    if (num > 7); 
+    
+    {
+        console.log(num);
+    }
+
+}
+
+teste_02(6);
+teste_02(8);
