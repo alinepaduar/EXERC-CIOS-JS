5c72a4d4f08a7901a9d2f872a5f7af850387a051
Aline de Pádua <paduarosa.aline@gmail.com>
2018-07-05 09:45:09 -0300

    Fundamentos OO
    Aula sobre OO em JS.

---
+++ Objeto/oo.js
@@ -0,0 +1,23 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Paradigma: Procedural
+processamento(valor1, valor2, valor3)
+
+// Paradigma: OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ... 
+    }
+}
+
+objeto.processamento(); // Foco passou a ser objeto.
+
+// Príncipios Importantes:
+// 1. Abstração : Um objeto do mundo real e como traduzir para dentro do sistema. Ex: Carro (Peso, Largura, Altura, Ligar, Desligar, Placa, etc...).
+/* 2. Encapsulamento : Ter detalhes de implementação escondidos e mostrar pra quem precisa usar, uma interface para interagir com aquele objeto. 
+    Ex: Detalhes internos do carro, mas a interface de comunicação com o carro é importante. */
+// 3. Herança (Prototype) : Recebe atributos e comportamentos de um objeto pai.
+// 4. Polimorfismo : Múltiplas formas. A partir de uma variável do tipo Carro e é capaz de atribuir uma Ferrari, Uno, BMW. Atribuir subtipos a variável Pai.
